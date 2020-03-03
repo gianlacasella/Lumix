45063563ffb1c8ad077308bea614c9f528f125af
glacasellaUANDES <glacasella@miuandes.cl>
2020-03-03 08:43:15 -0300

    Cambio en la ventana de login

--- iFruitUserLogin.Designer.cs
+++ iFruitUserLogin.Designer.cs
@@ -248,7 +248,7 @@
             this.Controls.Add(this.LogInPasswordTextBox);
             this.Controls.Add(this.UserNamesComboBox);
             this.Controls.Add(this.LogoPanel);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "iFruitUserLogin";
             this.Opacity = 0.9D;
